e35083dadc7f603044bfe9b2f2d68c2718a6b3b4
ArminCS97 <armin_felahatpisheh@edu.aua.am>
2019-12-10 22:05:39 +0400

    some endpoints are created and tested to show the data

--- Hotel/MyHotel/urls.py
+++ Hotel/MyHotel/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.index, name='index')
+    path("", views.index, name='index'),
+    path('RoomTypes', views.roomTypes),
+    path('Customers', views.customers),
 ]
--- Hotel/MyHotel/views.py
+++ Hotel/MyHotel/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import RoomTypes, Customers
 
 
 def index(request):
-    return render(request, 'MyHotel/index.html', {})
+    return render(request, 'MyHotel/ButtonsForQueries.html', {})
 
 
+def roomTypes(request):
+    return render(request, 'MyHotel/QueriesResults.html', {"code": RoomTypes.objects.all()})
+
+
+def customers(request):
+    return render(request, 'MyHotel/QueriesResults.html', {"code1": Customers.objects.all()})
